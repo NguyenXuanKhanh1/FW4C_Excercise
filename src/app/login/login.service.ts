9ae13d62de782785b1c9e7da8c73d0020ffe4d0d
Xuân Khánh Nguyễn <khanhbeobu2@gmail.com>
2019-10-23 10:11:00 +0700

    Check user login with correct username and password

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, Input } from '@angular/core';
+import { Login } from './login.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-
   constructor() { }
 
-  static passwordValidator(control) {
-    if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
+  static passwordValidator(login: Login) {
+    if (login.password.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
       return null;
     } else {
       return 'Invalid Password';
     }
+  }
+  checkLogin(login: Login) {
+    return (login.username === 'nxkhanh' && login.password === '123@123a');
   }
 }
